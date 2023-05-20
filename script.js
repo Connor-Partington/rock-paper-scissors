94436b932d6bf6412f4fdaff8dcd01c69bd93eb8
Connor <119803598+Connor-Partington@users.noreply.github.com>
2023-05-20 13:55:27 +0100

    Add function to check player and computer choice and return game outcome

--- script.js
+++ script.js
@@ -1,15 +1,26 @@
+// Create an array with each choice. Pick a random choice from the array and return the choice. 
 function getComputerChoice() {
-    const choices = ["Rock", "Paper", "Scissors"];
+    const choices = ["rock", "paper", "scissors"];
     let choice = choices[Math.floor(Math.random() * choices.length)];
     return choice;
 }
 
+// Check if player and computer choices match to a losing, winning or draw pair. 
 function playRound(playerSelection, computerSelection) {
-    // code
+    if (playerSelection === "rock" && computerSelection === "paper" || playerSelection === "paper" && computerSelection === "scissors" || playerSelection === "scissors" && computerSelection === "rock") {
+        return "You Lose!";
+    } else if (playerSelection === "rock" && computerSelection === "scissors" || playerSelection === "paper" && computerSelection === "rock" || playerSelection === "scissors" && computerSelection === "paper") {
+        return "You Win!";
+    } else {
+        return "It's a draw";
+    }
 }
 
-const playerSelection = "rock";
+// Ask player for their choice. 
+const playerSelection = prompt("What's your choice?", "Rock, Paper, Scissors")
 const computerSelection = getComputerChoice();
+
+// Print outcome in console. 
 console.log(playRound(playerSelection, computerSelection));
 
 function game() {
