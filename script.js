3418a64ddddaaf7ca4c73ab861ff57af4bd491bd
Connor <119803598+Connor-Partington@users.noreply.github.com>
2023-05-20 16:31:22 +0100

    Add scores and game results

--- script.js
+++ script.js
@@ -1,3 +1,8 @@
+let playerScore = 0;
+let computerScore = 0;
+let playerSelection = "";
+let computerSelection = getComputerChoice();
+
 // Create an array with each choice. Pick a random choice from the array and return the choice. 
 function getComputerChoice() {
     const choices = ["rock", "paper", "scissors"];
@@ -9,23 +14,29 @@ function getComputerChoice() {
 function playRound(playerSelection, computerSelection) {
     playerSelection = playerSelection.toLowerCase();
     if (playerSelection === "rock" && computerSelection === "paper" || playerSelection === "paper" && computerSelection === "scissors" || playerSelection === "scissors" && computerSelection === "rock") {
+        computerScore += 1;
         return "You Lose! " + playerSelection + " beats " + computerSelection;
     } else if (playerSelection === "rock" && computerSelection === "scissors" || playerSelection === "paper" && computerSelection === "rock" || playerSelection === "scissors" && computerSelection === "paper") {
+        playerScore += 1;
         return "You Win! " + playerSelection + " beats " + computerSelection;
     } else {
         return "It's a draw";
     }
 }
 
-let playerSelection = "";
-let computerSelection = getComputerChoice();
-
 function game() {
     for (i = 1; i < 6; i++) {
         playerSelection = prompt("Choose Rock, Paper, or Scissors");
         computerSelection = getComputerChoice();
-        console.log(i);
+        console.log("Round: " + i);
         console.log(playRound(playerSelection, computerSelection));
+    }
+    if (playerScore > computerScore) {
+        return "End of game. Player Wins!" + "\nPlayer score: " + playerScore + "\nComputer score: " + computerScore;
+    } else if (playerScore < computerScore) {
+        return "End of game. Player Loses!" + "\nPlayer score: " + playerScore + "\nComputer score: " + computerScore;
+    } else {
+        return "End of game. It's a draw" + "\nPlayer score: " + playerScore + "\nComputer score: " + computerScore;
     }
 }
 
